c53a2812a2def81b4b854244820aea31d2079015
saranafisa <sara_nafisa@protonmail.com>
2023-04-12 20:50:38 +0300

    done

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+ return tutorials.map(word => {
+ let arr= word.split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+return arr.join(' ')
+  });
 }
+console.log(titleCased())
